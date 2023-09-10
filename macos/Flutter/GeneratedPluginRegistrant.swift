f4a9bd9360c24ca59728bac446bc15835a05f116
ElSayed <elsayed.m.elareny@gmail.com>
2023-09-10 16:12:33 +0300

    update code

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,8 +9,8 @@ import assets_audio_player
 import assets_audio_player_web
 import geolocator_apple
 import location
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AssetsAudioPlayerPlugin.register(with: registry.registrar(forPlugin: "AssetsAudioPlayerPlugin"))
